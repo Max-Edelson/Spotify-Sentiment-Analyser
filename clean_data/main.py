f37cd3baf2a7489211103422a9294bea15eb0328
Maxim Edelson <medelson@ucsd.edu>
2020-12-03 09:54:21 -0800

    Cleaned data and a working clean-data algorithm

--- clean_data/main.py
+++ clean_data/main.py
@@ -1,11 +1,15 @@
 import csv
+import time
 
-pathToRawData = "../raw_data/"
-pathToLatestData = "../latest_data/"
-pathToCleanedData = "../cleaned_data/"
+start_time = time.time()
+
+pathToRawData = "../data/raw_data/"
+pathToLatestData = "../data/latest_data/"
+pathToCleanedData = "../data/cleaned_data/"
 
 # array that will contain arrays of Kaggle song data
-kaggleSongs = []
+# before removing unnecessary songs
+preCheckedKaggleSongs = []
 
 # array that will hold all of our top Spotify songs
 spotifySongs = []
@@ -13,8 +17,10 @@ spotifySongs = []
 KAGGLE_SONG_NAME_INDEX_NUMBER = 14
 TOP_SPOTIFY_SONG_NAME_INDEX_NUMBER = 1
 KAGGLE_VALENCE_INDEX = 0
+TOP_SPOTIFY_VALENCE_INDEX = 5
 KAGGLE_ARTIST_INDEX = 3
 TOP_SPOTIFY_ARTIST_INDEX = 2 
+KAGGLE_DATE_INDEX = 1
 
 # check if the song name has a 'featuring someone
 # section, if it does surrounded by "()", remove it
@@ -22,15 +28,19 @@ def removeParentheses(indexOfName, row):
 	foundParen = False
 	charCounter = 0
 	songName = row[indexOfName]
-	spotifySongName = ""
+	songNameB = ""
 	for char in songName:
 		if foundParen:
-			spotifySongName = songName[0:charCounter]
+			songNameB = songName[0:charCounter - 1]
+			break;
 		elif char == '(':
 			foundParen = True
 		charCounter += 1
+	if not foundParen:
+		songNameB = songName
+#	songNameB = songNameB.lower()
 	revisedSong = row
-	revisedSong[indexOfName] = spotifySongName.strip()
+	revisedSong[indexOfName] = songNameB.strip()
 
 	return revisedSong
 
@@ -39,51 +49,53 @@ def removeParentheses(indexOfName, row):
 # seperated by commas, this will remove the 
 # brackets, commas, and single quotation marks
 def cleanArtists(indexOfArtists, row):
+	cleanedArtists = []
+
 	artists = row[indexOfArtists]
 	badChars = ['[', ']', "'"]
 	for char in artists:
 		for i in badChars:
 			artists = artists.replace(i,'')
 	artists = artists.split(",")
-	[artist.strip() for artist in artists]
-	[artist.lower() for artist in artists]
+	for artist in artists:
+		artist = artist.lower()
+		artist = artist.strip()
+		cleanedArtists.append(artist)
 
-	return artists
+	return cleanedArtists
 
-# check if two rows are the same song
-def checkIfRowsMatch(spotifyRow, kaggleRow):
-	spotifyArtists = cleanArtists(TOP_SPOTIFY_ARTIST_INDEX, spotifyRow)
-	kaggleArtists = cleanArtists(KAGGLE_ARTIST_INDEX, kaggleRow)
+# check if the artists match
+def checkIfArtistsMatch(spotifySong, kaggleSong):
+	spotifyArtist = cleanArtists(TOP_SPOTIFY_ARTIST_INDEX, spotifySong)
+	kaggleArtists = cleanArtists(KAGGLE_ARTIST_INDEX, kaggleSong)
 
-	spotifySongName = removeParentheses(TOP_SPOTIFY_SONG_NAME_INDEX_NUMBER, spotifyRow)
-	kaggleSongName = removeParentheses(KAGGLE_SONG_NAME_INDEX_NUMBER, kaggleRow)
+	artistsMatch = False
+	for each in kaggleArtists:
+		if artistsMatch:
+			break
+		elif each == spotifyArtist[0]:
+			artistsMatch = True
+	
+	return artistsMatch
 
-#	print(spotifyArtists)
-#	print(kaggleArtists)
-#	print(spotifySongName)
-#	print(kaggleSongName)
-
-	songCounter = 1
-	sameSongs = False
-	for each in kaggleSongs:
-		if kaggleSongName == spotifySongName:
-			print("Song names are the same")
-			for each in kaggleArtists:
-				if each == spotifyArtists:
-					sameSongs = True
-					break
-		songCounter += 1
-	return sameSongs
+def checkIfSongNamesMatch(spotifySongName, kaggleSongName):
+	returnVal = False
+#	print(spotifySongName + " == " + kaggleSongName)
+	spotName = spotifySongName.lower()
+	kagName = kaggleSongName.lower()
+	if spotName == kagName:
+		returnVal = True
+	
+	return returnVal
 
 # read in the Kaggle data and remove extranneous
 # parts of the song name
 with open(pathToRawData + 'kaggle_data.csv') as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	for row in csv_reader :
-		songName = removeParentheses(KAGGLE_SONG_NAME_INDEX_NUMBER, row)
-		tempRow = row
-		tempRow[KAGGLE_SONG_NAME_INDEX_NUMBER] = songName
-		kaggleSongs.append(tempRow)
+		song = removeParentheses(KAGGLE_SONG_NAME_INDEX_NUMBER, row)
+		tempRow = song
+		preCheckedKaggleSongs.append(tempRow)
 
 
 # read in our top Spotify songs and remove extranneous 
@@ -91,22 +103,68 @@ with open(pathToRawData + 'kaggle_data.csv') as csv_file:
 with open(pathToLatestData + 'raw_files_combined.csv') as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	for row in csv_reader:
-		songName = removeParentheses(TOP_SPOTIFY_SONG_NAME_INDEX_NUMBER, row)
-		tempRow = row
-		tempRow[TOP_SPOTIFY_SONG_NAME_INDEX_NUMBER] = songName
+		song = removeParentheses(TOP_SPOTIFY_SONG_NAME_INDEX_NUMBER, row)
+		tempRow = song
 		spotifySongs.append(tempRow)
 	spotifySongs[0].append("Valence")
+	spotifySongs[0].append("Release Year")
 
-# go through spotifySongs and add the valence values
+#print(spotifySongs)
+
+# remove all songs from the Kaggle data set that aren't
+# in spotifySongs, doesn't account for duplicates
+# in spotifySongs and when you find a match, add in
+# the valence values to the spotify songs
+spotifySongCounter = 0
 for each in spotifySongs:
-	for kaggleSong in kaggleSongs:
-		if checkIfRowsMatch(each, kaggleSong) == True:
-			each.append(kaggleSong[KAGGLE_VALENCE_INDEX])
+	spotifySongCounter += 1
+	kaggleSongPointer = 0
+	kaggleSong = preCheckedKaggleSongs[kaggleSongPointer][KAGGLE_SONG_NAME_INDEX_NUMBER]
+	spotifySong = each[TOP_SPOTIFY_SONG_NAME_INDEX_NUMBER]
+	while checkIfSongNamesMatch(spotifySong, kaggleSong)  == False or checkIfArtistsMatch(each, preCheckedKaggleSongs[kaggleSongPointer]) == False:
+		if kaggleSongPointer < (len(preCheckedKaggleSongs) - 1):
+
+			kaggleSongPointer += 1
+			kaggleSong = preCheckedKaggleSongs[kaggleSongPointer][KAGGLE_SONG_NAME_INDEX_NUMBER]
+		else:
+			break
+
+#	if spotifySong.lower() == "havana" and kaggleSong.lower() == "havana":
+#		print(preCheckedKaggleSongs[kaggleSongPointer])
+	# do not add songs whose valence values are unknown
+#	print(f"spotifySong: {spotifySong}, kaggleSong: {kaggleSong}, kaggleSongPointer: {kaggleSongPointer}")
+	if checkIfSongNamesMatch(spotifySong, kaggleSong):
+		# songs match
+		kaggleValenceValue = preCheckedKaggleSongs[kaggleSongPointer][KAGGLE_VALENCE_INDEX]
+		if kaggleValenceValue != '' and float(kaggleValenceValue) >= 0 and float(kaggleValenceValue) <= 1:
+			each.append(kaggleValenceValue)
+			each.append(preCheckedKaggleSongs[kaggleSongPointer][KAGGLE_DATE_INDEX])
+	else:
+#		print(f"Kaggle data set does not contain {spotifySong}")
+		spotifySongs.pop(spotifySongCounter)
+		continue
+	if spotifySongCounter % 1000 == 0:
+		print(f"done with {spotifySongCounter} songs")
+
+# remove any songs that don't have a valence value
+counter = 0
+while counter < len(spotifySongs): 
+	if len(spotifySongs[counter]) < 7:
+		print(f"popping: {each[1]}")
+		spotifySongs.pop(counter)
+		counter = 0
+	else:
+		counter += 1
+
+
+#for each in spotifySongs:
+#	print(each)
 
 # writing to a csv file
 with open(pathToCleanedData + 'cleaned_data.csv', mode='w') as raw_files_combined:
 	file_writer = csv.writer(raw_files_combined, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 	for song in spotifySongs:
-		for songData in song:
-			file_writer.writerow(songData)
+		file_writer.writerow(song)
+
+print("Program took %s seconds to run." % (time.time() - start_time))
