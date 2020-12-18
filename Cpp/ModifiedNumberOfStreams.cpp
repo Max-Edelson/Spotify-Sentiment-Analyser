087a8a9275aff5816b0bb9686b874daeb6f858ba
Naweed-Malal <73226385+Naweed-Malal@users.noreply.github.com>
2020-12-18 14:28:59 -0800

    Add files via upload

--- Cpp/ModifiedNumberOfStreams.cpp
+++ Cpp/ModifiedNumberOfStreams.cpp
@@ -2,37 +2,96 @@
 #include <fstream>
 using namespace std;
 
+void modifiedNumberOfStreamsWeeks();
+void modifiedNumberOfStreamsMonths();
+
 int main() {
-	ifstream fileStreams;
-	int streams;
-	ifstream fileValenceMean;
-	double valenceMean;
+	//ifstream fileStreams;
+	//int streams;
+	//ifstream fileValenceMean;
+	//double valenceMean;
+	char choice;
 
-
-	fileStreams.open("streams.txt");
-	fileValenceMean.open("valence_mean.txt");
-
-
-	if (!fileStreams.is_open()) {
-		cout << "Can't open streams file" << endl;
-		system("pause");
-		exit(0);
+	cout << "Evaluate for weeks or months?\na:weeks\nb:months" << endl;
+	cin >> choice;
+	while (choice != 'a' && choice != 'b') {
+		cout << "Evaluate for weeks or months?\na:weeks\nb:months" << endl;
+		cin >> choice;
+	}
+	
+	if (choice == 'a') {
+		modifiedNumberOfStreamsWeeks();
 	}
 
-	if (!fileValenceMean.is_open()) {
-		cout << "Can't open valence_mean file" << endl;
-		system("pause");
-		exit(0);
-	}
-
-	int result;
-
-	while (fileStreams.good() && fileValenceMean.good()) {
-		fileStreams >> streams;
-		fileValenceMean >> valenceMean;
-
-		result = streams / (valenceMean + 1);
-		cout << result << endl;
-		
+	else {
+		modifiedNumberOfStreamsMonths();
 	}
 }
+
+	void modifiedNumberOfStreamsWeeks() {
+		ifstream fileStreams;
+		int streams;
+		ifstream fileValenceMean;
+		double valenceMean;
+
+		fileStreams.open("streams_weeks.txt");
+		fileValenceMean.open("valence_mean_weeks.txt");
+
+
+		if (!fileStreams.is_open()) {
+			cout << "Can't open streams_weeks file" << endl;
+			system("pause");
+			exit(0);
+		}
+
+		if (!fileValenceMean.is_open()) {
+			cout << "Can't open valence_mean_weeks file" << endl;
+			system("pause");
+			exit(0);
+		}
+
+		int result;
+
+		while (fileStreams.good() && fileValenceMean.good()) {
+			fileStreams >> streams;
+			fileValenceMean >> valenceMean;
+
+			result = streams / (valenceMean + 1);
+			cout << result << endl;
+
+		}
+	}
+
+	void modifiedNumberOfStreamsMonths() {
+		ifstream fileStreams;
+		int streams;
+		ifstream fileValenceMean;
+		double valenceMean;
+
+		fileStreams.open("streams_months.txt");
+		fileValenceMean.open("valence_mean_months.txt");
+
+
+		if (!fileStreams.is_open()) {
+			cout << "Can't open streams_months file" << endl;
+			system("pause");
+			exit(0);
+		}
+
+		if (!fileValenceMean.is_open()) {
+			cout << "Can't open valence_mean_months file" << endl;
+			system("pause");
+			exit(0);
+		}
+
+		int result;
+
+		while (fileStreams.good() && fileValenceMean.good()) {
+			fileStreams >> streams;
+			fileValenceMean >> valenceMean;
+
+			result = streams / (valenceMean + 1);
+			cout << result << endl;
+
+		}
+	}
